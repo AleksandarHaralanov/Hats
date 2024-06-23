645fddfb9796e2f50c1093bf5bd6245b087cd8d3
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-06-24 00:43:53 +0300

    Grab plugin.yml version instead of hardcoded version

--- src/main/java/me/beezle/hats/Hats.java
+++ src/main/java/me/beezle/hats/Hats.java
@@ -2,16 +2,20 @@ package me.beezle.hats;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import static org.bukkit.Bukkit.getLogger;
+
 public class Hats extends JavaPlugin {
+    String version;
 
     @Override
     public void onEnable() {
-        getCommand("hat").setExecutor(new HatsCommand());
-        System.out.print("[Hats v2.0.0] Enabled.");
+        version = this.getDescription().getVersion();
+        getCommand("hat").setExecutor(new HatsCommand(version));
+        getLogger().info(String.format("[Hats v%s] Enabled.", version));
     }
 
     @Override
     public void onDisable() {
-        System.out.print("[Hats v2.0.0] Disabled.");
+        getLogger().info(String.format("[Hats v%s] Enabled.", version));
     }
 }

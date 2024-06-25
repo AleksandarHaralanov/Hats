6cb88b56492690853cdb9490307003eab07e054e
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-06-25 07:28:48 +0300

    Imported custom update checker, updated how variables are used to display logs

--- src/main/java/me/beezle/hats/Hats.java
+++ src/main/java/me/beezle/hats/Hats.java
@@ -1,22 +1,32 @@
 package me.beezle.hats;
 
+import me.beezle.extras.UpdateUtil;
+
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 
 import static org.bukkit.Bukkit.getLogger;
 
 public class Hats extends JavaPlugin {
 
-    private static String version;
+    private static String currentVersion;
+    private static String pluginName;
 
     @Override
     public void onEnable() {
-        version = this.getDescription().getVersion();
-        getCommand("hat").setExecutor(new HatsCommand(version));
-        getLogger().info(String.format("[Hats] v%s Enabled.", version));
+        PluginDescriptionFile pdf = this.getDescription();
+        currentVersion = pdf.getVersion();
+        pluginName = pdf.getName();
+
+        getLogger().info(String.format("[%s] v%s Enabled.", pluginName, currentVersion));
+
+        UpdateUtil.checkForUpdates("https://api.github.com/repos/AleksandarHaralanov/Hats/releases/latest", currentVersion, pluginName);
+
+        getCommand("hat").setExecutor(new HatsCommand(currentVersion));
     }
 
     @Override
     public void onDisable() {
-        getLogger().info(String.format("[Hats] v%s Disabled.", version));
+        getLogger().info(String.format("[%s] v%s Disabled.", pluginName, currentVersion));
     }
 }

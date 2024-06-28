db9ebbf8fb96cac808464d826eb1cc01b1317bf8
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-06-28 09:56:05 +0300

    Modify to work with the now implemented ColorUtil in Poseidon newest build. Changed how to work with the new parameters.

--- src/main/java/com/haralanov/hats/HatsCommand.java
+++ src/main/java/com/haralanov/hats/HatsCommand.java
@@ -1,7 +1,5 @@
 package com.haralanov.hats;
 
-import com.haralanov.utilities.ColorUtil;
-
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,19 +7,23 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
 import org.bukkit.material.MaterialData;
+import org.bukkit.ChatColor;
 
 import static org.bukkit.Bukkit.getLogger;
-
-import java.util.Objects;
 
 public class HatsCommand implements CommandExecutor {
 
     String currentVersion;
     String pluginName;
+    String pluginAuthor;
+    String source;
 
-    public HatsCommand(String currentVersion, String pluginName) {
+    public HatsCommand(String currentVersion, String pluginName, String pluginAuthor, String githubUrl) {
         this.currentVersion = currentVersion;
         this.pluginName = pluginName;
+        this.pluginAuthor = pluginAuthor;
+        this.source = githubUrl.replace("api.github.com/repos", "github.com");
+        this.source = this.source.replace("/releases/latest", "");
     }
 
     @Override
@@ -33,13 +35,13 @@ public class HatsCommand implements CommandExecutor {
 
         if (command.getName().equalsIgnoreCase("hats")) {
             if (player != null) {
-                commandSender.sendMessage(ColorUtil.translateAlternateColorCodes('&',
-                        String.format("&e%s v%s &bby &eAleksandar Haralanov (Beezle)", pluginName, currentVersion)));
-                commandSender.sendMessage(ColorUtil.translateAlternateColorCodes('&',
-                        "&bSource: &egithub.com/AleksandarHaralanov/Hats"));
+                commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        String.format("&e%s v%s &bby &e%s", pluginName, currentVersion, pluginAuthor)));
+                commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        String.format("&bSource: &e%s", source)));
             } else {
-                getLogger().info(String.format("%s v%s by Aleksandar Haralanov (Beezle)", pluginName, currentVersion));
-                getLogger().info("Source: github.com/AleksandarHaralanov/Hats");
+                getLogger().info(String.format("%s v%s by %s", pluginName, currentVersion, pluginAuthor));
+                getLogger().info(String.format("Source: &e%s", source));
             }
 
         } else {
@@ -49,10 +51,10 @@ public class HatsCommand implements CommandExecutor {
             }
 
             if (!(commandSender.hasPermission("hats.wear") || commandSender.isOp())) {
-                commandSender.sendMessage(ColorUtil.translateAlternateColorCodes('&',
+                commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&',
                         "&cYou do not have permissions to wear hats."));
             } else {
-                this.hatWear(player, Objects.requireNonNull(player).getItemInHand());
+                this.hatWear(player, player.getItemInHand());
             }
 
         }
@@ -61,16 +63,16 @@ public class HatsCommand implements CommandExecutor {
 
     public void hatWear(Player player, ItemStack item) {
         if (item.getType() == Material.AIR) {
-            player.sendMessage(ColorUtil.translateAlternateColorCodes('&',
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                     "&cYou are not holding anything."));
         } else {
             int itemId = item.getTypeId();
 
             if (!(itemId >= 1 && itemId <= 96)) {
-                player.sendMessage(ColorUtil.translateAlternateColorCodes('&',
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                         String.format("&e%s &ccannot be worn as a hat.", item.getType().name())));
             } else if (player.getInventory().getHelmet().getType() == item.getType()) {
-                player.sendMessage(ColorUtil.translateAlternateColorCodes('&',
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                         String.format("&e%s &cis already worn as a hat.", item.getType().name())));
             } else {
                 PlayerInventory inventory = player.getInventory();
@@ -89,7 +91,7 @@ public class HatsCommand implements CommandExecutor {
                     } else {
                         removeExact(inventory, item);
                     }
-                    player.sendMessage(ColorUtil.translateAlternateColorCodes('&',
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                             String.format("&e%s &bworn as a hat.", item.getType().name())));
                 } else if (inventory.firstEmpty() != -1) {
                     inventory.addItem(helmet);
@@ -99,7 +101,7 @@ public class HatsCommand implements CommandExecutor {
                     } else {
                         removeExact(inventory, item);
                     }
-                    player.sendMessage(ColorUtil.translateAlternateColorCodes('&',
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                             String.format("&e%s &bhat swapped for &e%s &bas the new hat.", helmet.getType().name(), item.getType().name())));
                 } else {
                     player.getWorld().dropItemNaturally(player.getLocation(), helmet);
@@ -109,9 +111,9 @@ public class HatsCommand implements CommandExecutor {
                     } else {
                         removeExact(inventory, item);
                     }
-                    player.sendMessage(ColorUtil.translateAlternateColorCodes('&',
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                             String.format("&cNo pocket space, dropped old &e%s &chat to the ground.", helmet.getType().name())));
-                    player.sendMessage(ColorUtil.translateAlternateColorCodes('&',
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                             String.format("&e%s &bworn as the new hat.", item.getType().name())));
                 }
             }
